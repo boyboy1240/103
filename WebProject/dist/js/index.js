e15a7a0b6e4f785d6b4fb4796fa1c81bd40777a7
john <boyboy1240@gmail.com>
2021-01-03 14:02:51 +0800

    5

---
+++ WebProject/dist/js/index.js
@@ -0,0 +1,14 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if (toggle && nav) {
+      toggle.addEventListener('click', () => {
+        nav.classList.toggle('show');
+       
+      });
+    }
+  };
+  
+  showMenu('toggle', 'nav');
+  
